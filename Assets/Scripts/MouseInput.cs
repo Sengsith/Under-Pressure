078b7155a2eb7aac8e2d635ee986550b798374b9
Angelo Rosario <angelorosario10@gmail.com>
2023-08-03 12:59:41 -0700

    Mouse position
    Implemented mouse position in world space

--- Assets/Scripts/MouseInput.cs
+++ Assets/Scripts/MouseInput.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class MouseInput : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class MouseInput : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   private PlayerInputActions playerInputActions;
+   private Vector2 mouseInput;
+   private Vector3 mouseWorldPosition;
+
+   private void Awake () {
+      playerInputActions = new PlayerInputActions();
+
+      playerInputActions.Player.Mouse.Enable();
+      playerInputActions.Player.Grab.Enable();
+   }
+
+   private void Update () {
+      HandleMouseInput();
+   }
+
+   // Reads mouse position on the screen then converts it to world point.
+   private void HandleMouseInput () {
+      mouseInput = playerInputActions.Player.Mouse.ReadValue<Vector2>();
+
+      // Convert x and y to local space, but make the z = 0 so it is on same plane as game objects
+      mouseWorldPosition = new Vector3(
+         Camera.main.ScreenToWorldPoint(mouseInput).x,
+         Camera.main.ScreenToWorldPoint(mouseInput).y,
+         0f);
+
+      Debug.Log("Mouse position: " + mouseWorldPosition);
+   }
+
 }
